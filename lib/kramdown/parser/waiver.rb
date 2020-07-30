26390385cbc6f66faeecce2de07bb63eb7cb332c
Martin Kost <bonekost@gmail.com>
2020-07-30 14:48:30 +0200

    input parser

--- lib/kramdown/parser/waiver.rb
+++ lib/kramdown/parser/waiver.rb
@@ -7,19 +7,20 @@ module Kramdown
       def initialize(source, options)
         super
         text_input_parser = :text_input
-        @block_parsers << :text_input
-      end
-
-      def parse
-        super
+        @span_parsers << :text_input
       end
 
       INPUT_MATCH = /\w* = __/.freeze
-      define_parser(:text_input, INPUT_MATCH)
+      INPUT_NAME = /\w*/.freeze
 
       def parse_text_input
-        byebug
+        result = @src.scan(INPUT_MATCH)
+        saved_pos = @src.save_pos
+        input_name = INPUT_NAME.match(result).to_s
+        el = Element.new(:html_element, :input, {name: input_name, id: input_name})
+        @tree.children << el
       end
+      define_parser(:text_input, INPUT_MATCH)
     end
   end
 end
